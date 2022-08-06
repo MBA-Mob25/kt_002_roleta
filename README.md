e6548c0e737680d209137510cb923b76dc40deb2
Marcelo Mussi <mussi@prodev.com.br>
2022-08-06 10:07:00 -0300

    Atividade da Roleta da Sorte

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# kt_002_roleta
+# Roleta da Sorte
+versionCode: "1"
+versionName: "1.0.1"
+
+# Introdução
+Este aplicativo foi desenvolvido para fins academicos na aula de Desenvolvimento Android com Kotlin<br>
+Temos como objetivo ter uma aplicação focado no jogo Roleta da Sorte, onde você poderá escolher um número de 0 até 10 e o sistema irá, de forma randomica, sortear um número. Se você acertar terá uma mensagem de sucesso, caso não, você perdeu!
+
+![image](images/evidencia.png)
--- app/src/main/java/com/example/kt_003_roleta/MainActivity.kt
+++ app/src/main/java/com/example/kt_003_roleta/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.kt_003_roleta
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
-import kotlin.math.absoluteValue
 
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,13 +22,35 @@ class MainActivity : AppCompatActivity() {
 
             // Carregando o elemento onde o resultado será exibido
             val responseView = findViewById<TextView>(R.id.sorted_value)
-            // Sorteando um valor aleatório através da biblioteca do Java
-            val sorted = Random().nextInt(2)
+            // Carregando o valor preenchido pelo usuário
+            val numberValue = findViewById<EditText>(R.id.number)
 
-            // Definindo o valor final contendo o resultado do valor sorteado
-            print(sorted.absoluteValue.toString())
-            responseView.text = getString(R.string.sorted_value) + " " + sorted.absoluteValue
+            // Verificando se o valor foi preenchido
+            if (numberValue.text.isEmpty()) {
+                // Retornando um texto mostrando que o valor deve ser preenchido
+                responseView.text = getString(R.string.error_empty)
+                return@setOnClickListener
+            }
+
+            // Capturando o valor preenchido pelo usuário
+            val choiceNumber = numberValue.text.toString().toInt()
+
+            // Verificando se o número preenchi esta contido no range permitido
+            if (choiceNumber in 11 downTo -1) {
+                // Sorteando um valor aleatório através da biblioteca do Java
+                // Neste caso ele irá sortear valores de 0 até 10
+                val sorted = Random().nextInt(11)
+                println("Número sorteado foi $sorted e o usuáruo escolheu $choiceNumber")
+
+                // Definindo o valor final contendo o resultado do valor sorteado
+                if (sorted.toString() == numberValue.text.toString()) {
+                    responseView.text = getString(R.string.sorted_match)
+                } else {
+                    responseView.text = getString(R.string.sorted_not_match)
+                }
+            } else {
+                responseView.text = getString(R.string.range_error)
+            }
         }
     }
-
 }
