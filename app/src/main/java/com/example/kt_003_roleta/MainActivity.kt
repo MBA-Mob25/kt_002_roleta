fc7c5f1d6689246acae2b33e9ecc8f8a136db0e9
Marcelo Mussi <mussi@prodev.com.br>
2022-08-06 09:12:40 -0300

    Cometando código da parte 1 do exercício

--- app/src/main/java/com/example/kt_003_roleta/MainActivity.kt
+++ app/src/main/java/com/example/kt_003_roleta/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.example.kt_003_roleta
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Carregando o elemento do Botão contido na tela
         val random = findViewById<Button>(R.id.btn_submit)
+
+        // Definido uma ação ao clicar no botão "Rodar"
         random.setOnClickListener {
+
+            // Carregando o elemento onde o resultado será exibido
             val responseView = findViewById<TextView>(R.id.sorted_value)
+            // Sorteando um valor aleatório através da biblioteca do Java
             val sorted = Random().nextInt(2)
 
+            // Definindo o valor final contendo o resultado do valor sorteado
             responseView.text = getString(R.string.sorted_value) + sorted.toString()
         }
     }
+
 }
